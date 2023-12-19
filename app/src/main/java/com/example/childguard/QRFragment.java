c946f29fabd167166901d5be0d9e20e0207ed307
Asura146 <kyo008g@gmail.com>
2023-12-19 12:01:40 +0900

    WIP

--- app/src/main/java/com/example/childguard/QRFragment.java
+++ app/src/main/java/com/example/childguard/QRFragment.java
@@ -1,5 +1,6 @@
 package com.example.childguard;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,12 +55,20 @@ public class QRFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr, container, false);
+        View view = inflater.inflate(R.layout.fragment_qr, container, false);
+
+        Button cameraButton = view.findViewById(R.id.camera);
+
+
+        return view;
     }
+
 }
