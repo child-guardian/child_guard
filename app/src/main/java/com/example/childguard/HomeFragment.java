6402452f6b3857152669e1c303319c93f61f3065
ろむねこ <66072112+r-ca@users.noreply.github.com>
2024-01-11 16:20:31 +0900

    Revert "Kyou/layout"

--- app/src/main/java/com/example/childguard/HomeFragment.java
+++ app/src/main/java/com/example/childguard/HomeFragment.java
@@ -7,14 +7,11 @@ import android.os.Bundle;
 
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -69,18 +66,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        //bluetooth設定ボタンの処理
-        View view=inflater.inflate(R.layout.fragment_home,container,false);
-        MainActivity activity = (MainActivity) getActivity();
-
-        Button bt1=view.findViewById(R.id.Bluetooth_setup);
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                replaceFragment(new bluetooth_setupFragment());
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
@@ -103,19 +89,5 @@ public class HomeFragment extends Fragment {
             situationTextView.setText("\n乗車状態");
             situation_bg.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.frame_style_orange, null));
         }
-
-    }
-    //画面遷移メソッド
-    private void replaceFragment(Fragment fragment){
-        // フラグメントマネージャーの取得
-        FragmentManager manager = getParentFragmentManager(); // アクティビティではgetSupportFragmentManager()?
-        // フラグメントトランザクションの開始
-        FragmentTransaction transaction = manager.beginTransaction();
-        // レイアウトをfragmentに置き換え（追加）
-        transaction.replace(R.id.fragmentContainerView, fragment);
-        // 置き換えのトランザクションをバックスタックに保存する
-        transaction.addToBackStack(null);
-        // フラグメントトランザクションをコミット
-        transaction.commit();
     }
 }
